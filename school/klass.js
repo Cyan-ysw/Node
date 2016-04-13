239f6caf4ffb48e19be264597214d0ea9f0758f0
osc_cyan <cyan@androifi.com>
2016-04-13 22:26:24 +0800

    init

---
+++ school/klass.js
@@ -0,0 +1,17 @@
+var student = require('./student')
+var teacher = require('./teacher')
+
+function add(teacherName,students) {
+	teacher.add(teacherName)
+
+	students.forEach(function(item,index) {
+		student.add(item);
+	})
+
+}
+
+//是module的幅度方法
+exports.add = add;
+
+// 真实存在的
+// module.exports = add
